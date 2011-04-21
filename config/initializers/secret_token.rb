ecd827f070bcbee11d2fecf1ed60e800287a0c05
M@ McCray <darthapo@gmail.com>
2011-04-20 19:20:02 -0500

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Comicus::Application.config.secret_token = '96149815cb17b851f8aa1f51923f39d6cf3c0cabad206bce1069be689483a83ff5c4e3e7da99fbae5d970a2525433df2f2511e2081518472ecccf8b4d07b15b0'
